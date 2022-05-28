ad35fd2b75a4e3e74d8728221c50f494e45b7530
Tobias Kreuss <tobiaskreuss@hotmail.de>
2022-05-28 18:26:09 +0200

    refactor: refactored actions using command design pattern

--- action_scripts/resizeImage.py
+++ action_scripts/resizeImage.py
@@ -4,35 +4,38 @@ from utils.executionStatus import ExecutionStatus, Status
 
 # action specific imports
 from PIL import Image
+from action_scripts.action import Action
 
 
-def resizeImage(parameters, session_id):
-    """
-    :param parameters: already parsed and checked parameters
-    :param session_id: already validated session id of the user
-    """
-    images = get_from_image_root(session_id)
-    status = ExecutionStatus()
+class CustomAction(Action):
+    @staticmethod
+    def execute(parameters, session_id):
+        """
+            :param parameters: already parsed and checked parameters
+            :param session_id: already validated session id of the user
+            """
+        images = get_from_image_root(session_id)
+        status = ExecutionStatus()
 
-    # read parameters
-    cutout = parameters['cutout']
-    width = cutout['width']
-    height = cutout['height']
-    point_x = cutout['positionX']
-    point_y = cutout['positionY']
-    new_images = []
-    for file in images:
-        image_format = file.split('.')[-1]
-        image = Image.open(file)
+        # read parameters
+        cutout = parameters['cutout']
+        width = cutout['width']
+        height = cutout['height']
+        point_x = cutout['positionX']
+        point_y = cutout['positionY']
+        new_images = []
+        for file in images:
+            image_format = file.split('.')[-1]
+            image = Image.open(file)
 
-        try:
-            image = image.crop(
-                (point_x, point_y, point_x + width, point_y + height))
-            new_images.append(image)
-        except Exception as e:
-            status.set_status("Error: Image could not be resized")
-            return status
+            try:
+                image = image.crop(
+                    (point_x, point_y, point_x + width, point_y + height))
+                new_images.append(image)
+            except Exception as e:
+                status.set_status("Error: Image could not be resized")
+                return status
 
-    save_pillow_images(new_images, image_format, session_id)
-    status.set_status(Status.SUCCESS)
-    return status
+        save_pillow_images(new_images, image_format, session_id)
+        status.set_status(Status.SUCCESS)
+        return status
